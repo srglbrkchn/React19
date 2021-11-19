8aa107a895e351ca0f7a0486de59f3b68909af96
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-19 17:56:29 +0100

    Use map function and external function to double elements in an array and return a new array.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
+function double(x) {
+    return 2 * x;
+}
 
+const newNumbers = numbers.map(double);
+console.log(newNumbers);
 //Filter - Create a new array by keeping the items that return true.
 
 //Reduce - Accumulate a value by doing something to each item in an array.
