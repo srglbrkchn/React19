950762fb70d60e69aa852fe2c48ddb89ce0cded1
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-19 18:50:14 +0100

    Use find function to find a first function that matches our condition.

--- src/index.js
+++ src/index.js
@@ -7,28 +7,22 @@ var numbers = [3, 56, 2, 48, 5];
 // * 2); }); console.log(newNumbers); Filter - Create a new array by keeping the
 // items that return true. const newNumbers =  numbers.filter((number) => {
 // if(number < 10){         return number;     } }); let newNumbers = [];
-// numbers.forEach(number => {     if(number >10) {
-// newNumbers.push(number);     } });
+// numbers.forEach(number => {     if(number >10) { newNumbers.push(number);
+// } }); Reduce - Accumulate a value by doing something to each item in an
+// array. let newNumber = 0; numbers.forEach(number => {     newNumber +=
+// number; }); const newNumber = numbers.reduce((summation, currentNumb ) => {
+//   console.log("Sum: " + summation);     console.log("Current number: " +
+// currentNumb);     return summation += currentNumb; });
+// console.log(newNumber); 
 
 
-
-// Reduce - Accumulate a value by doing something to each item in an array. 
-
-// let newNumber = 0;
-
-// numbers.forEach(number => {
-//     newNumber += number;
-// });
-
-const newNumber = numbers.reduce((summation, currentNumb ) => {
-    console.log("Sum: " + summation);
-    console.log("Current number: " + currentNumb);
-
-    return summation += currentNumb;
+// Find find the first item that matches from an array.
+const newNumber = numbers.find(number =>{
+    if(number > 10){
+        return number;
+    }
 });
 
-
-// Find find the first item that matches from an array. FindIndex - find the index
-// of the first item that matches.
-
 console.log(newNumber);
+
+// FindIndex - find the index of the first item that matches.
