e7e739595677cdf4e591b6819822cf954cc8a3c9
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-19 22:33:37 +0100

    Create a new array, emojiMeaning out of emojiPedia by using map function and storing 100 chars capped mianing of each emoji in the new array.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-var numbers = [3, 56, 2, 48, 5];
-
-// Map -Create a new array by doing something with each item in an array.
-// function double(x) {     return 2 * x; } const newNumbers = numbers.map(x =>
-// {     return 2 * x; }); console.log(newNumbers); let newNumbers = [];
-// numbers.forEach((number) => {     newNumbers.push(number
+// var numbers = [3, 56, 2, 48, 5]; Map -Create a new array by doing something
+// with each item in an array. function double(x) {     return 2 * x; } const
+// newNumbers = numbers.map(x => {     return 2 * x; });
+// console.log(newNumbers); let newNumbers = []; numbers.forEach((number) => {
+//   newNumbers.push(number
 // * 2); }); console.log(newNumbers); Filter - Create a new array by keeping the
 // items that return true. const newNumbers =  numbers.filter((number) => {
 // if(number < 10){         return number;     } }); let newNumbers = [];
-// numbers.forEach(number => {     if(number >10) { newNumbers.push(number);
-// } }); Reduce - Accumulate a value by doing something to each item in an
-// array. let newNumber = 0; numbers.forEach(number => {     newNumber +=
-// number; }); const newNumber = numbers.reduce((summation, currentNumb ) => {
-//   console.log("Sum: " + summation);     console.log("Current number: " +
+// numbers.forEach(number => {     if(number >10) { newNumbers.push(number); }
+// }); Reduce - Accumulate a value by doing something to each item in an array.
+// let newNumber = 0; numbers.forEach(number => {     newNumber += number; });
+// const newNumber = numbers.reduce((summation, currentNumb ) => {
+// console.log("Sum: " + summation);     console.log("Current number: " +
 // currentNumb);     return summation += currentNumb; });
-// console.log(newNumber); 
+// console.log(newNumber); Find find the first item that matches from an array.
+// const newNumber = numbers.findIndex(number =>{     if(number > 10){ return
+// number;     } }); // FindIndex - find the index of the first item that
+// matches. console.log(newNumber);
 
+import emojies from "./emojipedia";
 
-// Find find the first item that matches from an array.
-const newNumber = numbers.findIndex(number =>{
-    if(number > 10){
-        return number;
-    }
+const emojiMeaning = emojies.map(emoji => {
+    return emoji.meaning.slice(0,100);
 });
 
-console.log(newNumber);
-
-// FindIndex - find the index of the first item that matches.
+console.log(emojiMeaning);
