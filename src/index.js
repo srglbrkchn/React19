f4603c21261eeb854de4b1fe2f9ff75d200a85fc
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-19 18:29:46 +0100

    Use filter function on numbers array to make a new array based on specific condition.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 var numbers = [3, 56, 2, 48, 5];
 
 // Map -Create a new array by doing something with each item in an array.
-function double(x) {
-    return 2 * x;
-}
-const newNumbers = numbers.map(x => {
-    return 2 * x;
+// function double(x) {     return 2 * x; } const newNumbers = numbers.map(x =>
+// {     return 2 * x; }); console.log(newNumbers); let newNumbers = [];
+// numbers.forEach((number) => {     newNumbers.push(number
+// * 2); }); console.log(newNumbers);
+
+// Filter - Create a new array by keeping the items that return true. 
+
+const newNumbers =  numbers.filter((number) => {
+    if(number < 10){
+        return number;
+    }
 });
 
 console.log(newNumbers);
 
-// let newNumbers = []; numbers.forEach((number) => {     newNumbers.push(number
-// * 2); }); console.log(newNumbers); Filter - Create a new array by keeping the
-// items that return true. Reduce - Accumulate a value by doing something to
+
+// Reduce - Accumulate a value by doing something to
 // each item in an array. Find - find the first item that matches from an array.
 // FindIndex - find the index of the first item that matches.
