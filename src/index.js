3416c4e7566e1e11a60a147b6f09655ee3812bf0
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-19 18:01:56 +0100

    Use forEach function to repaint the map functionality in doubling an array.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 var numbers = [3, 56, 2, 48, 5];
 
 //Map -Create a new array by doing something with each item in an array.
-function double(x) {
-    return 2 * x;
+// function double(x) {
+//     return 2 * x;
+// }
+
+// const newNumbers = numbers.map(double);
+// console.log(newNumbers);
+
+let newNumbers = [];
+
+function double(number) {
+    newNumbers.push(number * 2);
 }
 
-const newNumbers = numbers.map(double);
+numbers.forEach(double);
+
 console.log(newNumbers);
+
+
 //Filter - Create a new array by keeping the items that return true.
 
 //Reduce - Accumulate a value by doing something to each item in an array.
