29a7c9aa390b5eedffd263f49394761529723df8
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-11-19 18:08:35 +0100

    Make double function inside map function, a fat arrow function for convinience.

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
 var numbers = [3, 56, 2, 48, 5];
 
-//Map -Create a new array by doing something with each item in an array.
-// function double(x) {
-//     return 2 * x;
-// }
-
-// const newNumbers = numbers.map(double);
-// console.log(newNumbers);
-
-let newNumbers = [];
-
-function double(number) {
-    newNumbers.push(number * 2);
+// Map -Create a new array by doing something with each item in an array.
+function double(x) {
+    return 2 * x;
 }
-
-numbers.forEach((number) =>  {
-    newNumbers.push(number * 2);
+const newNumbers = numbers.map(x => {
+    return 2 * x;
 });
 
 console.log(newNumbers);
 
-
-//Filter - Create a new array by keeping the items that return true.
-
-//Reduce - Accumulate a value by doing something to each item in an array.
-
-//Find - find the first item that matches from an array.
-
-//FindIndex - find the index of the first item that matches.
+// let newNumbers = []; numbers.forEach((number) => {     newNumbers.push(number
+// * 2); }); console.log(newNumbers); Filter - Create a new array by keeping the
+// items that return true. Reduce - Accumulate a value by doing something to
+// each item in an array. Find - find the first item that matches from an array.
+// FindIndex - find the index of the first item that matches.
